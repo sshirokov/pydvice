dcc64b9e75c10a7103d1a5d4e864b7b1d3adfe3e
Yaroslav Shirokov <slava@slavasaur.com>
2011-01-09 05:14:06 -0500

    [master] Starting to write codez

--- lib/pydvice.py
+++ lib/pydvice.py
@@ -1,1 +1,18 @@
-MAGIC=13370
+
+class before(object):
+    '''Definition of before advice'''
+    def __init__(self, fun, **options):
+        options = dict({}, **options)
+        self.fun = fun
+
+    def run(self, *args, **kwargs):
+        print "Should run before advice:", self.advice
+        print "Then call:", self.fun
+        return self.fun(*args, **kwargs)
+
+    def __call__(self, advice):
+        self.advice = advice
+
+        self.run.__func__.__name__ = self.fun.__name__
+        self.run.__func__.__doc__  = self.fun.__doc__
+        return self.run
--- test.py
+++ test.py
@@ -14,6 +14,56 @@ except ImportError:
     ))
     import pydvice
 
+class Boilerplate(object):
+    def attach(self, fn):
+        setattr(self, fn.__name__, fn)
+        return fn
+
+    def setUp(self):
+        @self.attach
+        def identity(o):
+            '''The identity function'''
+            return o
+
+        @self.attach
+        def add1(n):
+            '''+1-er'''
+            return n + 1
+
+        @self.attach
+        def sum2(a, b):
+            '''Return: a + b'''
+            return a + b
+
+        @self.attach
+        def nothing(): '''I do nothing'''
+
+
+    def tearDown(self):
+        pass
+
+##Test Cases##
+class BeforeTests(Boilerplate, unittest.TestCase):
+    def test_sanity(self):
+        self.assertTrue(True, "True must be true")
+        self.assertFalse(False, "False must be false")
+        self.assertTrue(pydvice, "Must have pydvice to test")
+
+    def test_before_simple(self):
+        trace = {'ran': False}
+        meta = {'doc': self.nothing.__doc__,
+                'name': self.nothing.__name__}
+
+        @pydvice.before(self.nothing)
+        def store_trace():
+            trace['ran'] = True
+
+        self.assertEqual(self.nothing.__name__, meta['name'],
+                         "Name of function mustn't change when advised.")
+        self.assertTrue(self.nothing.__doc__.startswith(meta['doc']),
+                        "The docstring must be the same for at least the beginning.")
+        self.assertTrue(trace['ran'], "The advice should have ran")
+
 
 if __name__ == '__main__':
     unittest.main()
