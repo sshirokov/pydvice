d0c02b2b7600a6936c01f3a0f3563f4ccf9da420
Yaroslav Shirokov <slava@slavasaur.com>
2011-01-09 04:27:25 -0500

    [master] Test pacakge loader

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
+import sys, os
+import unittest
+
+#Try 'just' importing pydvice,
+# failing try looking in ./lib
+# next to this file
 try:
     import pydvice
-    print "Loaded without looking"
 except ImportError:
-    import sys, os
     sys.path.append(os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
         'lib'
     ))
     import pydvice
-    print "Loaded by guessing where lib is"
-except ImportError:
-    print "Failed to load the library!"
-    exit(0xff)
