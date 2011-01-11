23a212175b1f57e8fec233413f5b4eb1befdac55
Yaroslav Shirokov <slava@slavasaur.com>
2011-01-11 01:19:41 -0500

    [master] Polluting less

--- lib/pydvice.py
+++ lib/pydvice.py
@@ -62,12 +62,16 @@ class pydvice(object):
 class BaseAdvice(object):
     _meta = None
     active = None
+    options = {'activate': True}
 
+    advice = None
     fun = None
     fun_ref = None
+    shadow_name = None
+    pydvice = pydvice
 
     def __init__(self, fun, **options):
-        self.options = dict({'activate': True},
+        self.options = dict(self.options,
                             **options)
         self.shadow_name = '__advice_shadow_%s' % uuid.uuid4().hex
 
@@ -103,13 +107,13 @@ class BaseAdvice(object):
         else: return self.fun(*a, **k)
 
     def bind(self):
-        self.call_expr = dict(expr='lambda *a, **k: {magic} and {shadow}.run(*a, **k)',
-                              shadow=self.shadow_name,
-                              bound=False,
-                              magic='0xface',
-                              freevars=[])
-        while not self.call_expr['bound']:
-            caller = eval(compile(self.call_expr['expr'].format(**self.call_expr),
+        call_expr = dict(expr='lambda *a, **k: {magic} and {shadow}.run(*a, **k)',
+                         shadow=self.shadow_name,
+                         bound=False,
+                         magic='0xface',
+                         freevars=[])
+        while not call_expr['bound']:
+            caller = eval(compile(call_expr['expr'].format(**call_expr),
                                   '<pydvice.%s>' % self.position, 'eval'))
             try: self.fun_ref.func_code = types.FunctionType(caller.func_code,
                                                              self.fun_ref.func_globals,
@@ -119,16 +123,16 @@ class BaseAdvice(object):
             except ValueError, e:
                 if 'closure of length' not in str(e): raise
 
-                self.call_expr['freevars'].append('__%s' % uuid.uuid4().hex)
-                self.call_expr.update(
+                call_expr['freevars'].append('__%s' % uuid.uuid4().hex)
+                call_expr.update(
                     expr=reduce(lambda acc, var: \
-                                    '(lambda {var}: {expr})(True)'.format(**dict(self.call_expr, var=var, expr=acc)),
-                                self.call_expr['freevars'],
-                                self.call_expr['expr']),
-                    magic='(%s,)' % ','.join(self.call_expr['freevars'])
+                                    '(lambda {var}: {expr})(True)'.format(**dict(call_expr, var=var, expr=acc)),
+                                call_expr['freevars'],
+                                call_expr['expr']),
+                    magic='(%s,)' % ','.join(call_expr['freevars'])
                 )
             else:
-                self.call_expr['bound'] = True
+                call_expr['bound'] = True
         self.fun_ref.func_globals.update(**{self.shadow_name: self})
         return self
 
